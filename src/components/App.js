cbb49aa2d53d3e56e2412a9eff43f49a66f11777
Balram Gupta <109209618+balramg7@users.noreply.github.com>
2023-09-11 18:29:14 +0530

    error resolve

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
 import '../styles/App.css';
+import Header from './Header';
 const App = () => {
 
 
   return (
     <div id="main">
+      <Header />
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,33 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    entry: './src/index.js',
-
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+  output: {
+    path: path.join(__dirname, "/dist"), // the bundle output path
+    filename: "bundle.js", // the name of the bundle
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html", // to import index.html file inside index.js
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
     ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    }
+  },
 };
